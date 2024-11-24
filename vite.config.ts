5c722cadf32a7bcbaa229da1c83b64b168da8159
theJotne <pwachsmuth@gmx.de>
2024-11-24 17:43:18 +0100

    added github pages config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from 'tailwindcss'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Wochenplan_client",
   plugins: [react()],
   css: {
     postcss: {
